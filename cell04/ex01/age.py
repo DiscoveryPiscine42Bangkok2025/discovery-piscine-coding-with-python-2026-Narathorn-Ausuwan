c261b25880d02337b15c50b838ea8646ae4d6fe5
MatchaDesu <67070242@kmitl.ac.th>
2026-02-09 18:09:05 +0700

    cell04 update

---
+++ cell04/ex01/age.py
@@ -0,0 +1,6 @@
+age = int(input("Please tell me your age: "))
+
+print("You are currently",age,"years old.")
+print("In 10 years, your'll be",age+10,"years old.")
+print("In 20 years, your'll be",age+20,"years old.")
+print("In 30 years, your'll be",age+30,"years old.")
---
+++ cell04/ex03/float.py
@@ -0,0 +1,6 @@
+num = float(input("Give me a number: "))
+
+if num == int(num) :
+    print("This number is integer.")
+else :
+    print("This number is decimal.")
